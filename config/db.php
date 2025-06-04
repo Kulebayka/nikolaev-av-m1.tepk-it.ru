10adc48679626d879ee888eaad15d35d7a809f03
Николаев Артём Владимирович <nikolaev-av@college.tepk.dev>
2025-06-04 14:19:07 +0700

    Это моя работа

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=nikolaev-av_m1',
+    'username' => 'nikolaev-av',
+    'password' => 'PassWord648!',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
